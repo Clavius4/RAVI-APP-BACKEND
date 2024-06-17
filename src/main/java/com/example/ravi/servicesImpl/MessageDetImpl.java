d443492e6c3e8534df2550341b566ef32a155442
Paul Tome <120785679+Clavius4@users.noreply.github.com>
2024-06-17 16:07:12 +0300

    Changed recipient number to Ravis

--- src/main/java/com/example/ravi/servicesImpl/MessageDetImpl.java
+++ src/main/java/com/example/ravi/servicesImpl/MessageDetImpl.java
@@ -60,7 +60,7 @@ public class MessageDetImpl extends MessageDetService {
                                 "Tax District: " + district
                 );
                 List<Recipient> recipients = new ArrayList<>();
-                recipients.add(new Recipient(1, "255782671763"));
+                recipients.add(new Recipient(1, "255781969373"));
                 receivedMessage.setRecipients(recipients);
                 ResponseMessage responseFromDownstream = smsIntegrationService.getResponseFromDownstream(receivedMessage);
 
